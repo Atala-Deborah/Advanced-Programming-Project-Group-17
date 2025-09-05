53abe1b8a244840171aa92c379f1dcb6d137ea62
naomimunezero <157587546+naomimunezero@users.noreply.github.com>
2025-09-06 00:22:06 +0300

    Update Facility.php

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -1,1 +1,37 @@
-<!-- to be worked on by Naomi -->
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Facility extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'FacilityId';
+
+    protected $fillable = [
+        'Name',
+        'Location',
+        'Description',
+        'PartnerOrganization',
+        'FacilityType',
+        'Capabilities'
+    ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class, 'FacilityId', 'FacilityId');
+    }
+
+    public function equipment()
+    {
+        return $this->hasMany(Equipment::class, 'FacilityId', 'FacilityId');
+    }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class, 'FacilityId', 'FacilityId');
+    }
+}<!-- to be worked on by Naomi -->
